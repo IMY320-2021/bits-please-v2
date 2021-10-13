3bc0a3e9f4bc88ddd362ce20308abc3248a878fc
Christoff <christoff.linde@gmail.com>
2021-10-13 19:01:29 +0200

    Add global Layout component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Layout.css";
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Heading 1</h1>
+      </div>
+    );
+  }
+}
+
+export default Layout;
