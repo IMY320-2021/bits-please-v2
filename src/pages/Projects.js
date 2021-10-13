c054509f04418a549fb2100b306465ffa585b7b9
Christoff <christoff.linde@gmail.com>
2021-10-13 18:38:12 +0200

    Add pages and basic routing

---
+++ src/pages/Projects.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+export default function Projects() {
+    return (
+        <div className="App">
+
+        </div>
+    )
+}
