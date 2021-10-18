891b64d21782ec1068c466adf3c96aeff1ec9b64
christoff-linde <christoff.linde@gmail.com>
2021-10-18 18:42:52 +0200

    Add PropType checking

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,15 @@
-/* eslint-disable react/prop-types */
 import React from "react";
+import PropTypes from "prop-types";
 
 import "../../App.css";
 import "./Layout.css";
 
 const Layout = ({ children }) => {
   return <div className="global-wrapper">{children}</div>;
+};
+
+Layout.propTypes = {
+  children: PropTypes.any,
 };
 
 export default Layout;
