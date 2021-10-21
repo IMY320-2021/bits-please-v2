881ed155dc83a534380e3eead49eba1f02ee6039
christoff-linde <christoff.linde@gmail.com>
2021-10-21 21:14:45 +0200

    Style page titles to correspond with home page colours

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -5,7 +5,7 @@ import Layout from "../components/Layout/Layout";
 export default function Shop() {
   return (
     <Layout>
-      <h1>Merch Store</h1>
+      <h1 className="color-salmon">Merch Store</h1>
       <CardContainer />
     </Layout>
   );
