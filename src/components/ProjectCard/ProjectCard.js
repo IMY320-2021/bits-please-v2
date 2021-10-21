ae9a99a643e09df8481aba4ab91557e8c9e82b27
christoff-linde <christoff.linde@gmail.com>
2021-10-22 01:42:19 +0200

    Update styling & propType validation

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -4,10 +4,9 @@ import PropTypes from "prop-types";
 import "./ProjectCard.css";
 
 const ProjectCard = ({ ...props }) => {
-  console.log(props);
   return (
-    <div className="project-card">
-      <h2 className="color-salmon">{props.title}</h2>
+    <div className="project-card" onClick={props.viewProject}>
+      <h4 className="color-salmon">{props.title}</h4>
       <img src={props.image_url} alt={"undefined"} />
       <p>{props.description}</p>
     </div>
@@ -18,6 +17,7 @@ ProjectCard.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   image_url: PropTypes.string,
+  viewProject: PropTypes.any,
 };
 
 export default ProjectCard;
