fb2ab57a93595811e404f0c6ffe1a6890da5b8f0
Jd Perumal <u18219285@tuks.co.za>
2021-10-22 01:15:11 +0200

    Implemented back navigation for ContactUs page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -2,12 +2,29 @@ import React from "react";
 
 import Layout from "../components/Layout/Layout";
 import SignupForm from "../components/SignupForm/SignupForm";
+import { Link } from "react-router-dom";
 
 export default function ContactUs() {
   return (
     <div className="App">
       <Layout>
-        <h1>Contact Us</h1>
+        <div style={{ display: "flex" }}>
+          <Link
+            className="nes-btn is-primary"
+            to="/Home"
+            style={{
+              paddingTop: 10,
+              paddingBottom: 10,
+              width: "10%",
+              height: "5%",
+            }}
+          >
+            {"< Back"}
+          </Link>
+          <h1 style={{ paddingTop: 10, paddingBottom: 10, paddingLeft: "30%" }}>
+            Contact Us
+          </h1>
+        </div>
         <SignupForm />
       </Layout>
     </div>
