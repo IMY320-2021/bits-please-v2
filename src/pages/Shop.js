55f4807558b0fe51a925b14cd3a1e6f6a3b1da41
Jd Perumal <u18219285@tuks.co.za>
2021-10-22 01:18:57 +0200

    Implemented back navigation for MerchStore page

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,11 +1,31 @@
 import React from "react";
 import CardContainer from "../components/CardContainer/CardContainer";
 import Layout from "../components/Layout/Layout";
+import { Link } from "react-router-dom";
 
 export default function Shop() {
   return (
     <Layout>
-      <h1 className="color-salmon">Merch Store</h1>
+      <div style={{ display: "flex" }}>
+        <Link
+          className="nes-btn is-primary"
+          to="/Home"
+          style={{
+            paddingTop: 10,
+            paddingBottom: 10,
+            width: "10%",
+            height: "5%",
+          }}
+        >
+          {"< Back"}
+        </Link>
+        <h1
+          style={{ paddingTop: 10, paddingBottom: 10, paddingLeft: "30%" }}
+          className="color-salmon"
+        >
+          Merch Store
+        </h1>
+      </div>
       <CardContainer />
     </Layout>
   );
