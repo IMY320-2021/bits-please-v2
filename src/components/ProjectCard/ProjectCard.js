e564567b71354575f27fe639e7e233ff223b834c
christoff-linde <christoff.linde@gmail.com>
2021-10-22 09:36:23 +0200

    Update PropType validation & expanded components to be more dynamic

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -2,22 +2,40 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import "./ProjectCard.css";
+import { object } from "prop-types";
 
 const ProjectCard = ({ ...props }) => {
+  const data = props.data;
+
   return (
     <div className="project-card" onClick={props.viewProject}>
-      <h4 className="color-salmon">{props.title}</h4>
-      <img src={props.image_url} alt={"undefined"} />
-      <p>{props.description}</p>
+      <h4 className="color-salmon">{data.title}</h4>
+      <p>{data.summary}</p>
+      <img src={data.image_url} alt={"undefined"} />
+      <p>{data.description}</p>
+      <h4 className="color-green">Tech Stack</h4>
+      {data.tech.map((tech, index) => {
+        return (
+          <React.Fragment key={index}>
+            <h6 className="color-yellow">{tech.name}</h6>
+            <p>{tech.description}</p>
+          </React.Fragment>
+        );
+      })}
     </div>
   );
 };
 
 ProjectCard.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
-  image_url: PropTypes.string,
-  viewProject: PropTypes.any,
+  data: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image_url: PropTypes.string.isRequired,
+    tech: PropTypes.arrayOf(object).isRequired,
+  }).isRequired,
+  viewProject: PropTypes.func.isRequired,
 };
 
 export default ProjectCard;
