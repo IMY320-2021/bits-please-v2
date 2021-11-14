d7ae8534b9de4578c50d7fb198d4b5801ce2f2ce
Christoff <christoff.linde@gmail.com>
2021-11-14 17:52:00 +0200

    Update ProjectPreviewCard to display badges

--- src/components/ProjectPreviewCard/ProjectPreviewCard.js
+++ src/components/ProjectPreviewCard/ProjectPreviewCard.js
@@ -4,11 +4,24 @@ import PropTypes from "prop-types";
 import "./ProjectPreviewCard.css";
 
 const ProjectPreviewCard = ({ ...props }) => {
+  const data = props.data;
+
   return (
     <div className="project-preview-card" onClick={props.viewProject}>
-      <h4 className="color-salmon">{props.title}</h4>
+      <h4 className="">{data.title}</h4>
+      <div className="badge-container">
+        {data.tags.map((tag, index) => {
+          return (
+            // eslint-disable-next-line jsx-a11y/anchor-is-valid
+            <a href={undefined} className="nes-badge" key={index}>
+              <span className={`${tag.type}`}>{tag.title}</span>
+            </a>
+          );
+        })}
+      </div>
+
       <p>{props.summary}</p>
-      <img src={props.image_url} alt={"undefined"} />
+      <img src={data.image_url} alt={"undefined"} />
     </div>
   );
 };
@@ -17,6 +30,12 @@ ProjectPreviewCard.propTypes = {
   data: PropTypes.shape({
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    tags: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+      })
+    ).isRequired,
     summary: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     image_url: PropTypes.string.isRequired,
