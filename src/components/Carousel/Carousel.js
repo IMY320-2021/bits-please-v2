14f637c82af9ebb19989d63734190c69d5fd66e2
Christoff <christoff.linde@gmail.com>
2021-11-14 21:58:47 +0200

    Add swiper carousel
    Interaction was temporarily removed. This needs to be added back

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,7 +1,47 @@
 import React from "react";
 
-const Carousel = () => {
-  return <></>;
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
+
+import "swiper/swiper.min.css";
+import "swiper/swiper-bundle.min.css";
+import "swiper/modules/navigation/navigation.min.css";
+import "swiper/modules/pagination/pagination.min.css";
+
+import "./Carousel.css";
+
+import SwiperCore, { Keyboard, Pagination, Navigation } from "swiper";
+import ProjectPreviewCard from "../ProjectPreviewCard/ProjectPreviewCard";
+
+SwiperCore.use([Keyboard, Pagination, Navigation]);
+
+const Carousel = ({ ...props }) => {
+  const data = props;
+  console.log(data);
+
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        keyboard={{
+          enabled: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        className="mySwiper"
+      >
+        {data.data.map((project, index) => {
+          return (
+            <SwiperSlide key={project.id}>
+              <ProjectPreviewCard data={project} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
+  );
 };
 
 export default Carousel;
