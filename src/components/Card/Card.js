d4663a26762a68d0d329f11ff77ef9c5fabeab12
Jd Perumal <u18219285@tuks.co.za>
2021-11-18 15:25:17 +0200

    Converted Card class into Card function

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,38 +3,38 @@ import PropTypes from "prop-types";
 import CustomRadioGroup from "../CustomRadioGroup/CustomRadioGroup";
 import "./Card.css";
 
-class Card extends React.Component {
-  render() {
-    const data = this.props.data;
+// class Card extends React.Component {
+export default function Card({ data }) {
+  // render() {
 
-    return (
-      <div className="card">
-        {/* <div className="card nes-container is-dark"> */}
-        <div className="card-header">
-          <div className="card-image">
-            <img
-              className="object-fit_cover"
-              src={data.image_url}
-              alt={data.description}
-            />
-          </div>
-        </div>
-        <div className="card-body">
-          <h3 className="color-salmon">{data.title}</h3>
-          <p>{data.description}</p>
-          <h2 className="price-heading color-green">R {data.price}</h2>
-          <CustomRadioGroup data={data.options} />
-          <button className="nes-btn is-primary" type="button">
-            ADD TO CART
-          </button>
+  const product = data;
+
+  return (
+    <div className="card">
+      {/* <div className="card nes-container is-dark"> */}
+      <div className="card-header">
+        <div className="card-image">
+          <img
+            className="object-fit_cover"
+            src={product.image_url}
+            alt={product.description}
+          />
         </div>
       </div>
-    );
-  }
+      <div className="card-body">
+        <h3 className="color-salmon">{product.title}</h3>
+        <p>{product.description}</p>
+        <h2 className="price-heading color-green">R {product.price}</h2>
+        <CustomRadioGroup data={product.options} />
+        <button className="nes-btn is-primary" type="button">
+          ADD TO CART
+        </button>
+      </div>
+    </div>
+  );
+  //   }
 }
 
 Card.propTypes = {
   data: PropTypes.object,
 };
-
-export default Card;
