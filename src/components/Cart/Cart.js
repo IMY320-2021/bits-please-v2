be70e1a7aa3b46d3a6720d7c08f5844fe3f77324
Jd Perumal <u18219285@tuks.co.za>
2021-11-18 17:59:24 +0200

    Implemented custom close modal button styling

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,40 +10,63 @@ export default function Cart() {
   const onOpenModal = () => setOpen(true);
   const onCloseModal = () => setOpen(false);
 
-  const { isEmpty, totalUniqueItems, items, updateItemQuantity, removeItem } =
+  const { isEmpty, totalItems, items, updateItemQuantity, removeItem } =
     useCart();
+
+  const closeIcon = (
+    <button type="button" className="nes-btn">
+      <i className="nes-icon close is-small"></i>
+    </button>
+  );
 
   return (
     <>
       <div>
-        <button onClick={onOpenModal}>Cart</button>
+        <button
+          className="nes-btn is-primary"
+          type="button"
+          onClick={onOpenModal}
+        >
+          Cart ({totalItems})
+        </button>
         <Modal
           open={open}
           onClose={onCloseModal}
           center
+          closeIcon={closeIcon}
           classNames={{
             overlay: "customOverlay",
             modal: "customModal",
           }}
         >
-          <h1>Cart ({totalUniqueItems})</h1>
+          <h1>Cart ({totalItems})</h1>
           {isEmpty && <p>Your cart is empty</p>}
           <ul>
             {items.map((item) => (
               <li key={item.id}>
-                {item.quantity} x {item.title} &mdash;{" "}
-                {"R" + item.price + " Each "}
+                {item.title} &mdash; {"R" + item.price + " Each "}
                 <button
+                  className="nes-btn is-primary"
+                  type="button"
                   onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
                 >
                   -
                 </button>
+                {" " + item.quantity + " "}
                 <button
+                  className="nes-btn is-primary"
+                  type="button"
                   onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
                 >
                   +
                 </button>
-                <button onClick={() => removeItem(item.id)}>&times;</button>
+                <button
+                  className="nes-btn is-error"
+                  type="button"
+                  onClick={() => removeItem(item.id)}
+                >
+                  &times;
+                </button>
               </li>
             ))}
           </ul>
