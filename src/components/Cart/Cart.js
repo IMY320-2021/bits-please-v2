2d9caf71ad2c376eab8bc1e0ed9c34cf18577579
Jd Perumal <u18219285@tuks.co.za>
2021-11-18 17:32:53 +0200

    Implemented basic shopping cart modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCart } from "react-use-cart";
-
+import ReactDOM from "react-dom";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
 export default function Cart() {
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
   const { isEmpty, totalUniqueItems, items, updateItemQuantity, removeItem } =
     useCart();
 
@@ -9,25 +16,31 @@ export default function Cart() {
 
   return (
     <>
-      <h1>Cart ({totalUniqueItems})</h1>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            {item.quantity} x {item.title} &mdash; {"R" + item.price + " Each "}
-            <button
-              onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
-            >
-              -
-            </button>
-            <button
-              onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
-            >
-              +
-            </button>
-            <button onClick={() => removeItem(item.id)}>&times;</button>
-          </li>
-        ))}
-      </ul>
+      <div>
+        <button onClick={onOpenModal}>Open modal</button>
+        <Modal open={open} onClose={onCloseModal} center>
+          <h1>Cart ({totalUniqueItems})</h1>
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>
+                {item.quantity} x {item.title} &mdash;{" "}
+                {"R" + item.price + " Each "}
+                <button
+                  onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
+                >
+                  -
+                </button>
+                <button
+                  onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
+                >
+                  +
+                </button>
+                <button onClick={() => removeItem(item.id)}>&times;</button>
+              </li>
+            ))}
+          </ul>
+        </Modal>
+      </div>
     </>
   );
 }
