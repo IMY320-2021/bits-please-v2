ccf2c44a8a23819a774a0370f8f2ead0ea119a58
Jd Perumal <u18219285@tuks.co.za>
2021-11-18 18:42:02 +0200

    Implemented table for structured cart layout

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -41,36 +41,51 @@ export default function Cart() {
         >
           <h1>Cart ({totalItems})</h1>
           {isEmpty && <p>Your cart is empty</p>}
-          <ul>
-            {items.map((item) => (
-              <li key={item.id}>
-                {item.title} &mdash; {"R" + item.price + " Each "}
-                <button
-                  className="nes-btn is-primary"
-                  type="button"
-                  onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
-                >
-                  -
-                </button>
-                {" " + item.quantity + " "}
-                <button
-                  className="nes-btn is-primary"
-                  type="button"
-                  onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
-                >
-                  +
-                </button>
-                <button
-                  className="nes-btn is-error"
-                  type="button"
-                  onClick={() => removeItem(item.id)}
-                >
-                  &times;
-                </button>
-              </li>
-            ))}
-          </ul>
 
+          <table>
+            <thead>
+              <th>Item </th>
+              <th>Price each</th>
+              <th>Quantity</th>
+            </thead>
+            <tbody>
+              {items.map((item) => (
+                <tr key={item.id}>
+                  <td> {item.title} </td>
+                  <td> {"R" + item.price} </td>
+
+                  <td>
+                    <button
+                      className="nes-btn is-primary"
+                      type="button"
+                      onClick={() =>
+                        updateItemQuantity(item.id, item.quantity - 1)
+                      }
+                    >
+                      -
+                    </button>
+                    {" " + item.quantity + " "}
+                    <button
+                      className="nes-btn is-primary"
+                      type="button"
+                      onClick={() =>
+                        updateItemQuantity(item.id, item.quantity + 1)
+                      }
+                    >
+                      +
+                    </button>
+                    <button
+                      className="nes-btn is-error"
+                      type="button"
+                      onClick={() => removeItem(item.id)}
+                    >
+                      &times;
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
           <h3 className="total">Total {"R5000"}</h3>
           <button className="nes-btn is-success checkOut" type="button">
             Checkout
