8caab384e26f3ddddab31b2cf05314d53741a6ac
Christoff <christoff.linde@gmail.com>
2021-11-19 21:08:30 +0200

    Add additional products & update styling

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,20 +3,17 @@ import PropTypes from "prop-types";
 import CustomRadioGroup from "../CustomRadioGroup/CustomRadioGroup";
 import "./Card.css";
 import { useCart } from "react-use-cart";
-import toast, { Toaster } from "react-hot-toast";
 
 export default function Card({ data }) {
   const product = data;
-  const { addItem, inCart } = useCart();
-  const alreadyAdded = inCart(product.id);
-  const notify = () => toast.success("Added " + product.title + " to Cart!");
+  const { addItem } = useCart();
 
   return (
     <div className="card">
       <div className="card-header">
         <div className="card-image">
           <img
-            className="object-fit_cover"
+            className="object-fit_fill"
             src={product.image_url}
             alt={product.description}
           />
@@ -24,19 +21,17 @@ export default function Card({ data }) {
       </div>
       <div className="card-body">
         <h3 className="color-salmon">{product.title}</h3>
-        <p>{product.description}</p>
+        <div className="product-description">
+          <p>{product.description}</p>
+        </div>
         <h2 className="price-heading color-green">R {product.price}</h2>
-        <CustomRadioGroup data={product.options} />
+        {product.options && <CustomRadioGroup data={product.options} />}
         <button
           className="nes-btn is-primary"
           type="button"
-          onClick={() => {
-            addItem(product);
-            notify();
-          }}
+          onClick={() => addItem(product)}
         >
-          {alreadyAdded ? "Add again" : "Add to Cart"}
-          <Toaster />
+          ADD TO CART
         </button>
       </div>
     </div>
--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -6,13 +6,34 @@ import MugShot from "../../assets/images/store/mug.jpg";
 import Hoodie from "../../assets/images/store/hoodie.jpg";
 import LongTShirt from "../../assets/images/store/long-tshirt.jpg";
 import ShortTShirt from "../../assets/images/store/short-tshirt.jpg";
+import Arcade from "../../assets/images/store/arcade.jpg";
+import Usb from "../../assets/images/store/usb.jpg";
 
 const StoreData = {
   data: [
     {
+      id: "usb-01",
+      title: "Flash Drive",
+      description: "For coffee addicts, the perfect mug (coffee not included)",
+      price: 79.95,
+      image_url: [`${Usb}`],
+      options: [
+        { id: "mg-01-550", stock: true, option: "64Gb" },
+        { id: "mg-01-550", stock: true, option: "256Gb" },
+      ],
+    },
+    {
+      id: "hd-01",
+      title: "Mini Arcade",
+      description: "A Mini Arcade action figure collectable for your desk",
+      price: 199.95,
+      image_url: [`${Arcade}`],
+      options: [{ id: "arcade-sm", stock: true, option: "Small" }],
+    },
+    {
       id: "mg-01",
       title: "Mug",
-      description: "For coffee addicts, the perfect mug",
+      description: "For coffee addicts, the perfect mug (coffee not included)",
       price: 59.95,
       image_url: [`${MugShot}`],
       options: [
@@ -23,7 +44,7 @@ const StoreData = {
     {
       id: "hd-01",
       title: "Hoodie",
-      description: "A comfortable hoodie",
+      description: "A comfortable hoodie with a drawstring and front pocket",
       price: 359.95,
       image_url: [`${Hoodie}`],
       options: [
@@ -31,12 +52,11 @@ const StoreData = {
         { id: "hd-01-s", stock: true, option: "S" },
         { id: "hd-01-m", stock: true, option: "M" },
         { id: "hd-01-l", stock: true, option: "L" },
-        { id: "hd-01-xl", stock: true, option: "XL" },
       ],
     },
     {
       id: "s-ts-01",
-      title: "Short Sleeve T-Shirt",
+      title: "T-Shirt",
       description: "100% pure cotton short sleeve T-Shirt with printed graphic",
       price: 129.95,
       image_url: [`${ShortTShirt}`],
@@ -45,12 +65,11 @@ const StoreData = {
         { id: "s-ts-01-s", stock: true, option: "S" },
         { id: "s-ts-01-m", stock: true, option: "M" },
         { id: "s-ts-01-l", stock: true, option: "L" },
-        { id: "s-ts-01-xl", stock: true, option: "XL" },
       ],
     },
     {
       id: "l-ts-01",
-      title: "Long Sleeve T-Shirt",
+      title: "Sweatshirt",
       description: "100% pure cotton long sleeve T-Shirt with printed graphic",
       price: 159.95,
       image_url: [`${LongTShirt}`],
@@ -58,7 +77,6 @@ const StoreData = {
         { id: "ls-ts-01-s", stock: true, option: "S" },
         { id: "ls-ts-01-m", stock: true, option: "M" },
         { id: "ls-ts-01-l", stock: true, option: "L" },
-        { id: "ls-ts-01-xl", stock: true, option: "XL" },
       ],
     },
   ],
